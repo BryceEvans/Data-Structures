06f08bc240272597705d8ff7174a78449497ed75
BryceEvans <evans.bryce@gmail.com>
2019-06-24 21:21:15 -0600

    insert of BST completed.

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,17 +1,35 @@
 class BinarySearchTree:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value):
+        # the value at the current node
+        self.value = value
+        # reference to this node's left child
+        self.left = None
+        # reference to this node's right child
+        self.right = None
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        # 1. Check if the new node's value is less than
+        # a. Is there a child? If not, insert new Node
+        # b. Repeat the process (recursion)
+        if value <= self.value:
+            if not self.left:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
+        # 2. Check if new node's value is greater than
+        # a. Is there a child? If not, insert ner node
+        # b. Repeat the process (recursion)
+        elif value > self.value:
+            if not self.right:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
 
-  def contains(self, target):
-    pass
+    def contains(self, target):
+        pass
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        pass
 
-  def for_each(self, cb):
-    pass
+    def for_each(self, cb):
+        pass
