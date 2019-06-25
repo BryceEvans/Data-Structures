122d787a36f5effb02fb4661f01761b91847e6f0
BryceEvans <evans.bryce@gmail.com>
2019-06-24 22:18:15 -0600

    for_each

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -48,4 +48,12 @@ class BinarySearchTree:
             return self.right.get_max()
 
     def for_each(self, cb):
-        pass
+        # if left, go left
+        if self.left:
+            self.left.for_each(cb)
+        # if right, go right
+        if self.right:
+            self.right.for_each(cb)
+        cb(self.value)
+        
+        
