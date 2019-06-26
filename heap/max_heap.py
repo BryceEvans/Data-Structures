3f205c5109eb81d363185ee7fb4935642f682e59
BryceEvans <evans.bryce@gmail.com>
2019-06-25 20:47:58 -0600

    Group project.

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -1,21 +1,34 @@
 class Heap:
-  def __init__(self):
-    self.storage = []
+    def __init__(self):
+        self.storage = []
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        pass
 
-  def delete(self):
-    pass
+    def delete(self):
+        pass
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        pass
 
-  def get_size(self):
-    pass
+    def get_size(self):
+        pass
 
-  def _bubble_up(self, index):
-    pass
+    def _bubble_up(self, index):
+        # 1. Check to see if the index is greater than zero
+        # 2. Grab the parent index
+        # 3. Check if current value is greater than or less than parent value
+            # a. if current is greater than
+            # b. Swap
+        # 4. If current is lesser than parent
+            # a. leave it alone - break
+        while index > 0:
+            parent = (index - 1) // 2
+            if self.storage[index] > self.storage[parent]:
+                self.storage[index], self.storage[parent] = self.storage[parent], self.storage[index]
+                index = parent # updating your new index
+            else:
+                break
 
-  def _sift_down(self, index):
-    pass
+    def _sift_down(self, index):
+        pass
